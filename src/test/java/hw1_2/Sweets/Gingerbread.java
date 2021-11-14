251a68310359b2f8d8e28163e165e052905eb0ee
Andrey <dyxa1@mail.ru>
2021-11-14 22:30:05 +0300

    добавил дз по java

--- src/test/java/hw1_2/Sweets/Gingerbread.java
+++ src/test/java/hw1_2/Sweets/Gingerbread.java
@@ -2,15 +2,15 @@ package hw1_2.Sweets;
 
 public class Gingerbread extends Gift {
 
-    private final boolean glaze;
+    private final boolean hasGlaze;
 
-    public Gingerbread(String name, double weight, double price, boolean glaze) {
+    public Gingerbread(String name, double weight, double price, boolean hasGlaze) {
         super(name, weight, price);
-        this.glaze = glaze;
+        this.hasGlaze = hasGlaze;
     }
 
     @Override
     public String toString() {
-        return "Пряник: [" + super.toString() + ", Есть ли глазурь: " + glaze + "]";
+        return "Пряник: [" + super.toString() + ", Есть ли глазурь: " + hasGlaze + "]";
     }
 }
